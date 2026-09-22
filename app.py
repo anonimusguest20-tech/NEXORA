cbdc6d6dc032dee48117a6fa836cc4e535b18a91
TeaHack <tua@email.com>
2026-09-22 23:16:31 +0200

    fix deploy

--- app.py
+++ app.py
@@ -233,3 +233,12 @@ def admin():
 os.makedirs('instance', exist_ok=True)
 with app.app_context(): db.create_all()
 if __name__ == '__main__': app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+os.makedirs('instance', exist_ok=True)
+with app.app_context():
+    db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
